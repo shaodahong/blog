bbaaa484e196c2dead54b9912f51fdb69f44b61d
骗你是小猫咪 <darryshaw@gmail.com>
2021-06-28 11:58:29 +0000

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import 'nextra-theme-blog/style.css';
-import Head from 'next/head';
+import "nextra-theme-blog/style.css";
+import Head from "next/head";
 
-import '../styles/main.css';
+import "../styles/main.css";
 
 export default function Nextra({ Component, pageProps }) {
   return (
@@ -19,6 +19,19 @@ export default function Nextra({ Component, pageProps }) {
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
+        />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-175790070-1"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'UA-175790070-1');`,
+          }}
         />
       </Head>
       <Component {...pageProps} />
