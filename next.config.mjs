7d11e26e7ac84f459e8761f4000fca7f70dc1919
shaodahong <darryshaw@gmail.com>
2023-02-18 11:50:13 +0800

    improve tag pagr

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import nextra from "nextra";
+
+const withNextra = nextra({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.mjs",
+  staticImage: true,
+  defaultShowCopyCode: true,
+  readingTime: true,
+});
+
+export default withNextra({
+  reactStrictMode: true,
+});
--- theme.config.mjs
+++ theme.config.mjs
@@ -1,11 +1,12 @@
 const YEAR = new Date().getFullYear();
 
 export default {
+  darkMode: true,
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
+    <small style={{ display: "block", marginTop: "8rem" }}>
       <abbr
         title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
-        style={{ cursor: 'help' }}
+        style={{ cursor: "help" }}
       >
         CC BY-NC 4.0
       </abbr>
