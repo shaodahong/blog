c0fe43b65608649979cfdbbd489882849948fe60
shaodahong <darryshaw@gmail.com>
2023-07-23 15:09:01 +0800

    improce rss script and sort by date

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -1,41 +1,56 @@
-const { promises: fs } = require('fs');
-const path = require('path');
-const RSS = require('rss');
-const matter = require('gray-matter');
+const { promises: fs } = require("fs");
+const path = require("path");
+const RSS = require("rss");
+const matter = require("gray-matter");
+const fg = require("fast-glob");
 
 // @TODO: for now let's generate with all posts, unsorted
 async function generate() {
   const feed = new RSS({
-    title: 'Bie Wen',
-    site_url: 'https://biewen.me',
-    feed_url: 'https://biewen.me/feed.xml',
+    title: "Bie Wen",
+    site_url: "https://biewen.me",
+    feed_url: "https://biewen.me/feed.xml",
   });
 
-  const posts = await fs.readdir(
-    path.join(__dirname, '..', 'pages', 'posts', '2021'),
-  );
+  const posts = await fg([
+    path.join(__dirname, "..", "pages/posts", "*", "*.md"),
+    path.join(__dirname, "..", "pages/posts", "*", "*.mdx"),
+  ]);
 
-  await Promise.all(
-    posts.map(async (name) => {
-      if (name.startsWith('index.')) return;
+  const feeds = await Promise.all(
+    posts.map(async (postName) => {
+      const lastSlashIndex = postName.lastIndexOf("/");
+      const name = postName.substring(lastSlashIndex + 1);
 
-      const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', '2021', name),
-      );
+      if (name.startsWith("index.")) return;
+
+      const content = await fs.readFile(postName);
+
+      const a = content.toString();
+
       const frontmatter = matter(content);
 
-      feed.item({
+      return {
         title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
+        url: "/posts/" + name.replace(/\.mdx?/, ""),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
+        categories: frontmatter.data.tag?.split(", ") ?? "",
         author: frontmatter.data.author,
-      });
-    }),
+      };
+    })
   );
 
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }));
+  feeds
+    .filter(Boolean)
+    .sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    })
+    .forEach((feedItem) => {
+      feed.item(feedItem);
+    });
+
+  await fs.writeFile("./public/feed.xml", feed.xml({ indent: true }));
 }
 
 generate();
